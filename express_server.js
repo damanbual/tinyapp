d5c3befdfe47e9be2361e76e1443226ed7124312
Daman Bual <damanbual@gmail.com>
2024-06-24 15:46:55 -0700

    added username to header

--- express_server.js
+++ express_server.js
@@ -1,48 +1,68 @@
+
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// Middleware to parse request bodies
 app.use(express.urlencoded({ extended: true }));
 
+// Route to render the homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Route to send the URL database as JSON
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Route to send a simple Hello World HTML page
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Route to display the list of URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"] 
+  };
   res.render("urls_index", templateVars);
 });
 
+// Route to render the new URL submission form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
+// Route to display a single URL and its shortened form
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
+  const templateVars = {
+    id, 
+    longURL, 
+    username: req.cookies["username"] 
+  };
   res.render("urls_show", templateVars);
 });
 
+// Function to generate a random short URL ID
 const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
 };
 
+// Route to handle form submission for creating a new shortened URL
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
@@ -83,5 +103,5 @@ app.post("/login", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
