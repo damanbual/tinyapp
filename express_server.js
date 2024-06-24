35b1efea80e93d8f40f136c2c55b7d670eafda1d
Daman Bual <damanbual@gmail.com>
2024-06-24 15:37:40 -0700

    added login feature

--- express_server.js
+++ express_server.js
@@ -75,6 +75,13 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+// Route to handle user login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
